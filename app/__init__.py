4b7270e049fa59adf4b190aa55b37e80cedfedc3
Amanda Tan <atan50@stuy.edu>
2025-04-03 11:36:52 -0400

    connection to mongo?

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,11 @@ Time Spent: [INSERT TIME HERE]
 from flask import Flask, flash, render_template, request, redirect, url_for, session, flash
 import pymongo
 
+uri = "mongodb+srv://amandat109:NUcnFGlhEO1TdOaQ@blueberry.pjtpwgq.mongodb.net/?retryWrites=true&w=majority&appName=Blueberry"
+
 # MONGODB INITIALIZATION
-client = pymongo.MongoClient("mongodb://localhost:27017/")
+client = pymongo.MongoClient(uri, server_api=ServerApi('1'))
+#client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["database"]
 user_collection = db["users"]
 # print(client.list_database_names()) # printing all databases in system
