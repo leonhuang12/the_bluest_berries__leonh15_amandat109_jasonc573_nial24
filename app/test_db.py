f5a686771ccc98af4c1c4ffd947e0ef767b3f616
Jason Chao <jchao50@stuy.edu>
2025-04-07 11:48:04 -0400

    Added test mongo db functions

---
+++ app/test_db.py
@@ -0,0 +1,31 @@
+# import pymongo
+#
+# # MONGODB INITIALIZATION
+# connection = 'mongodb+srv://amandat109:NUcnFGlhEO1TdOaQ@blueberry.pjtpwgq.mongodb.net/?retryWrites=true&w=majority&appName=Blueberry'
+# client = pymongo.MongoClient(connection)
+# db = client['database']
+# user_collection = db['users']
+#
+# user_dict = {
+#     "username": "jason",
+#     "password": "chao"
+# }
+#
+# user_insertion = user_collection.insert_one(user_dict)
+# print(user_insertion.inserted_id)
+
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://jasonc573:Dragonace2010@blueberry.pjtpwgq.mongodb.net/?appName=Blueberry"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
