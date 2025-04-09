7f3cda1ee7e2cfac311e48a1cade81080b408803
Jason Chao <jchao50@stuy.edu>
2025-04-09 11:47:46 -0400

    Imported CSV into MongoDB

--- app/test_db.py
+++ app/test_db.py
@@ -4,6 +4,7 @@
 # connection = 'mongodb+srv://amandat109:NUcnFGlhEO1TdOaQ@blueberry.pjtpwgq.mongodb.net/?retryWrites=true&w=majority&appName=Blueberry'
 
 import pymongo
+import csv
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
@@ -19,12 +20,33 @@ except Exception as e:
 
 db = client['database']
 user_collection = db['users']
+student_data_collection = db['student_data']
 
-user_dict = {
-    "username": "jason",
-    "password": "chao"
-}
+with open("StudentPerformanceFactors.csv") as performance_data:
+    data = csv.reader(performance_data, delimiter=',')
+    next(data) # skip headers
+    for row in data:
+        student_data_dict = { # reordered dict info
+            'study_hours': row[0],
+            'sleep_hours': row[5],
+            'tutoring_sessions': row[9],
+            'physical_activity': row[14],
+            'attendance': row[1],
+            'gender': row[18],
+            'previous_score': row[6],
+            'score': row[19]
+        }
+        data_insertion = student_data_collection.insert_one(student_data_dict)
 
-user_insertion = user_collection.insert_one(user_dict)
-print(user_insertion.inserted_id)
-print(user_collection.find_one())
+
+# document_deletion = user_collection.delete_many({})
+# print(document_deletion.deleted_count, " documents deleted.")
+
+# user_dict = {
+#     "username": "jason",
+#     "password": "chao"
+# }
+#
+# user_insertion = user_collection.insert_one(user_dict)
+# print(user_insertion.inserted_id)
+# print(user_collection.find_one())
